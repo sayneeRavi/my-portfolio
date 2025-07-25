fb7e91dc2bdf1b8aa360d98fd943615cdc05a8d3
sayneeRavi <raveendrana.22@uom.lk>
2025-07-25 13:51:10 +0530

    add Eduction to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ const Navbar = () => {
     <nav className="w-full flex justify-between items-center p-4 bg-gray-100">
       <h1 className="text-xl font-bold">Athmi.dev</h1>
       <ul className="flex gap-6">
-    <a href="#about" className="cursor-pointer">About</a>
-
-        <li><a href="#projects" className="hover:underline">Projects</a></li>
-        <li><a href="#contact" className="hover:underline">Contact</a></li>
+        <li><a href="#about" className="hover:underline cursor-pointer">About</a></li>
+        <li><a href="#education" className="hover:underline cursor-pointer">Education</a></li>
+        <li><a href="#projects" className="hover:underline cursor-pointer">Projects</a></li>
+        <li><a href="#contact" className="hover:underline cursor-pointer">Contact</a></li>
       </ul>
     </nav>
   );
